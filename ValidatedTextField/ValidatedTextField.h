2e8293195909e3953271336d5d317a3898380a4b
Chris Chares <chrisc@activewebsite.com>
2013-11-14 15:53:47 -0700

    Initial Commit

---
+++ ValidatedTextField/ValidatedTextField.h
@@ -0,0 +1,13 @@
+//
+//  ValidatedTextField.h
+//  ValidatedTextField
+//
+//  Created by Chris Chares on 11/14/13.
+//  Copyright (c) 2013 Eunoia Design Co. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ValidatedTextField : NSObject
+
+@end
---
+++ ValidatedTextField/ValidatedTextField.m
@@ -0,0 +1,13 @@
+//
+//  ValidatedTextField.m
+//  ValidatedTextField
+//
+//  Created by Chris Chares on 11/14/13.
+//  Copyright (c) 2013 Eunoia Design Co. All rights reserved.
+//
+
+#import "ValidatedTextField.h"
+
+@implementation ValidatedTextField
+
+@end
